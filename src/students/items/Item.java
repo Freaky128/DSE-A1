aeb74c3cbe2732745bc1eb265ad3ea08459078e0
Freaky128 <matthewfreak@hotmail.com>
2023-03-31 17:04:31 +1030

    Made abstract Item class and basic functionality.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,51 @@
 package students.items;
 
-public class Item {
-
+abstract class Item {
+	private int age;
+	private int matureAge;
+	private int deathAge;
+	private int value;
 	
+	public Item() {
+		this.age = 0;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public int getMatureAge() {
+		return this.matureAge;
+	}
+	
+	public int getDeathAge() {
+		return this.deathAge;
+	}
+	
+	public void tick() {
+		this.age += 1;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean died() {
+		return this.age > this.deathAge;
+	}
+	
+	public int getValue() {
+		if (this.age > this.matureAge) {
+			return this.value;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public boolean equals(Item item ) {
+		return (this.age == item.getAge() && this.matureAge == item.getMatureAge() && this.deathAge == item.getDeathAge() && this.getValue() == item.getValue())
+	}
+	
+	public abstract String toString();
 }
