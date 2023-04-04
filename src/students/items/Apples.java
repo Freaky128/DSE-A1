77312964676b2768867d330f2ed0b658267a7eb5
Freaky128 <matthewfreak@hotmail.com>
2023-04-04 23:21:15 +0930

    Finished Apples class functionality (for now).

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,29 @@
 package students.items;
 
-public class Apples {
-
+public class Apples extends Food{
+	private static int genNum = 0;
+	//not sure this should be stored here
+	private static int cost = 2;
+	
+	public Apples() {
+		super();
+		genNum += 1;
+		this.matureAge = 3;
+		this.deathAge = 5;
+		this.value = 3;
+	}
+	
+	public String toString() {
+		if (this.age <  this.matureAge) {
+			return "a";
+		}
+		else {
+			return "A";
+		}
+	}
+	
+	public static int getGenerationCount() {
+		return genNum;
+	}
+	
 }
