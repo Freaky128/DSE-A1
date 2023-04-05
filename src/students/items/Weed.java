b5ecd5629c7c76e25f90d717014a5b522817dc0c
Freaky128 <matthewfreak@hotmail.com>
2023-04-05 14:00:11 +0930

    Finished Weed class functionality.

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -1,6 +1,16 @@
 package students.items;
 
-public class Weed
-{
-
+public class Weed extends Item{
+	public Weed() {
+		this.matureAge = -1;
+		this.deathAge = 1;
+		this.value = -1;
+	}
+	
+	@Override
+	public void tick() {}
+	
+	public String toString() {
+		return "#";
+	}
 }
