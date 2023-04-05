900d3cc75cab868803715d0aa5dd9285ad1c7efd
Freaky128 <matthewfreak@hotmail.com>
2023-04-05 14:05:04 +0930

    Finished UntilledSoil class functionality.

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,16 @@
 package students.items;
 
-public class UntilledSoil {
-
-
+public class UntilledSoil extends Item{
+	public UntilledSoil() {
+		this.matureAge = -1;
+		this.deathAge = 1;
+		this.value = -1;
+	}
+	
+	@Override
+	public void tick() {}
+	
+	public String toString() {
+		return "/";
+	}
 }
