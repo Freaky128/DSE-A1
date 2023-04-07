7cd3dff465e04c40c17e2e90601089fb40a22df1
Freaky128 <matthewfreak@hotmail.com>
2023-04-08 02:01:39 +0930

    Added Farm constructor and started run().

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -1,13 +1,36 @@
 package students;
+import java.util.Scanner;
 
 public class Farm {
+	private Field field;
+	private int balance;
+	private boolean running = true;
 	
 	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
 	{
+		this.field = new Field(fieldHeight, fieldWidth);
+		this.balance = startingFunds;
 	}
 	
 	public void run()
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to Sim Farm!\n");
+		
+		while (running) {
+			System.out.println(this.field + "\n");
+			System.out.println("Bank Balance: $" + this.balance + "\n");
+			System.out.println("Enter your next action:");
+			System.out.println("  t x y: till\n"
+								+ "  h x y: harvest\n"
+								+ "  p x y: plant\n"
+								+ "  s: field summary\n"
+								+ "  w: wait\n"
+								+ "  q: quit\n");
+			//input.
+			
+			running = false;
+		}
 	}
 	
 }
